a928a3bb1a5e89bd9013d6be9f12222a1c972e28
pJulianV <pjuliandvargas@gmail.com>
2023-06-08 18:30:22 -0500

    less requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 from io import BytesIO
-from pyxlsb import open_workbook as open_xlsb
+# from pyxlsb import open_workbook as open_xlsb
 import streamlit as st
 
 # import plotly.express as px
