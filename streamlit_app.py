1c441b1a1d84306b373208d0bd791da32beb4a54
pJulianV <pjuliandvargas@gmail.com>
2023-07-04 17:32:00 -0500

    prueba secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -466,5 +466,8 @@ def load_data(sheets_url):
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
+a = st.secrets["public_gsheets_url"]
+st.text(a)
+df
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
